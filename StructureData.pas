c647177246fd9a6620c252b073384605c71cd049
aufaf29 <47714476+aufaf29@users.noreply.github.com>
2019-04-07 15:40:02 +0700

    Update StructureData

--- StructureData.pas
+++ StructureData.pas
@@ -8,23 +8,23 @@ const
 	
 type
 	tanggal = record
-		DD 		: string;
-		MM 		: string;
+		DD 	: string;
+		MM 	: string;
 		YYYY 	: string;
 	end;
 	
 	buku = record
-		ID_Buku 		: string;
-		Judul_Buku 		: string;
-		Author			: string;
+		ID_Buku 	: string;
+		Judul_Buku	: string;
+		Author		: string;
 		Jumlah_Buku 	: string;
 		Tahun_Penerbit	: string;
-		Kategori		: string;
+		Kategori	: string;
 	end;
 	
 	tabBuku = record
 		tab		: array[arrMin..arrMax] of buku;
-		Neff	: integer;
+		Neff		: integer;
 	end;
 	
 	user = record
@@ -37,41 +37,41 @@ type
 	
 	tabUser = record
 		tab		: array[arrMin..arrMax] of user;
-		Neff	: integer;
+		Neff		: integer;
 	end;
 	
 	historyPeminjaman = record
-		Username 					: string;
-		ID_Buku						: string;
-		Tanggal_Peminjaman			: tanggal;
+		Username 			: string;
+		ID_Buku				: string;
+		Tanggal_Peminjaman		: tanggal;
 		Tanggal_Batas_Pengembalian	: tanggal;
-		Status_Pengembalian			: string;
+		Status_Pengembalian		: string;
 	end;
 
 	tabHistoryPeminjaman	= record
-		tab		: array[arrMin..arrMax] of historyPeminjaman;
+		tab	: array[arrMin..arrMax] of historyPeminjaman;
 		Neff	: integer;
 	end;
 
 	historyPengembalian = record
-		Username 					: string;
-		ID_Buku						: string;
-		Tanggal_Pengembalian		: tanggal;
+		Username 		: string;
+		ID_Buku			: string;
+		Tanggal_Pengembalian	: tanggal;
 	end;
 
 	tabHistoryPengembalian	= record
-		tab		: array[arrMin..arrMax] of historyPengembalian;
+		tab	: array[arrMin..arrMax] of historyPengembalian;
 		Neff	: integer;
 	end;
 	
 	laporanKehilangan  =  record
-		Username 		: string;
-		ID_Buku			: string;
+		Username 	: string;
+		ID_Buku		: string;
 		Tanggal_Laporan	: tanggal;
 	end;
 	
 	tabKehilangan  =  record
-		tab		: array[arrMin..arrMax] of laporanKehilangan;
+		tab	: array[arrMin..arrMax] of laporanKehilangan;
 		Neff	: integer;
 	end;
 
